9513c93d4a3cc07964dcfb36662b1cfd84661090
rohscx <rohscx@gmail.com>
2024-11-30 16:39:48 -0500

    Initial Commit

---
+++ SnapBarcodeLocator/SnapBarcodeLocatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapBarcodeLocatorApp.swift
+//  SnapBarcodeLocator
+//
+//  Created by Fohristiwhirl on 11/30/24.
+//
+
+import SwiftUI
+
+@main
+struct SnapBarcodeLocatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
